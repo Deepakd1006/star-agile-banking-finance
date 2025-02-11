6f34d9b216274cd9ae3e8625f7fe0c327d00271c
DEEPAK KUMAR D <98271570+Deepakd1006@users.noreply.github.com>
2025-02-11 22:43:21 +0530

    Update banking_terraform.tf

--- banking_terraform.tf
+++ banking_terraform.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # Infra-Structure readyness for project1 : Banking
 
 #Jenkins master Node : VM1
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "my_ec2_1" {
   ami               = "ami-023a307f3d27ea427" 
   instance_type     = "t2.micro" 
   associate_public_ip_address = true
@@ -19,7 +19,7 @@ resource "aws_instance" "my_ec2" {
 
 #Jenkins slave Node (Build server) VM2
 
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "my_ec2_2" {
   ami               = "ami-023a307f3d27ea427" 
   instance_type     = "t2.micro" 
   associate_public_ip_address = true
@@ -31,7 +31,7 @@ resource "aws_instance" "my_ec2" {
 
 # Kubernetes Master Node VM3
 
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "my_ec2_3" {
   ami               = "ami-023a307f3d27ea427" 
   instance_type     = "t2.micro" 
   associate_public_ip_address = true
@@ -43,7 +43,7 @@ resource "aws_instance" "my_ec2" {
 
 # Kubernetes Master Node VM4
 
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "my_ec2_4" {
   ami               = "ami-023a307f3d27ea427" 
   instance_type     = "t2.micro" 
   associate_public_ip_address = true
@@ -55,7 +55,7 @@ resource "aws_instance" "my_ec2" {
 
 # Kubernetes Master Node VM5
 
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "my_ec2_5" {
   ami               = "ami-023a307f3d27ea427" 
   instance_type     = "t2.micro" 
   associate_public_ip_address = true
@@ -67,7 +67,7 @@ resource "aws_instance" "my_ec2" {
 
 # Prometheus & Grafana Monitoring Server VM6
 
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "my_ec2_6" {
   ami               = "ami-023a307f3d27ea427" 
   instance_type     = "t2.micro" 
   associate_public_ip_address = true
